147b8d661bcad6b7bb7a6c5ac3f2de741df2c91b
杨俊 <756585379@qq.com>
2018-10-11 13:14:25 +0800

    no message

---
+++ 微信点餐系统/sell 4/src/main/java/com/imooc/service/SellerService.java
@@ -0,0 +1,14 @@
+package com.imooc.service;
+
+import com.imooc.dataobject.SellerInfo;
+
+/**
+ * 卖家端
+ */
+public interface SellerService {
+
+    /**
+     * 通过openid查询卖家端信息
+     */
+    SellerInfo findSellerInfoByOpenid(String openid);
+}
